1171ac4237249b94f2f62e24c08c85e10af99e50
BassamAlim <65797540+BassamAlim@users.noreply.github.com>
2023-06-21 03:22:52 +0300

    improvements

--- src/components/projects/ProjectGallery.jsx
+++ src/components/projects/ProjectGallery.jsx
@@ -6,33 +6,12 @@ const ProjectGallery = ({id}) => {
 
     return (
         <div className="grid grid-cols-1 sm:grid-cols-4 sm:gap-10 my-12 py-4 sm:py-8 sm:mt-20 border-y-2 border-primary-light dark:border-secondary-dark">
-            {
-                data.images.length > 1 ?
-                    data.images.map((image) => (
-                        image.direction === Direction.Vertical ?
-                            <div key={data.id}>
-                                <img
-                                    src={image.img}
-                                    className="rounded-xl cursor-pointer shadow-lg sm:shadow-none"
-                                    alt={data.title}
-                                    key={data.id}
-                                />
-                            </div>
-                        :
-                            <div key={data.id} className="col-span-2 flex justify-center text-center items-center">
-                                <img
-                                    src={image.img}
-                                    className="rounded-xl cursor-pointer shadow-lg sm:shadow-none"
-                                    alt={data.title}
-                                    key={data.id}
-                                />
-                            </div>
-                    ))
-                    :
-                    data.images[0].direction === Direction.Vertical ?
+            {  // bug here: the project images show in sub projects
+                data.images.map((image) => (
+                    image.direction === Direction.Vertical ?
                         <div key={data.id}>
                             <img
-                                src={data.images[0].img}
+                                src={image.img}
                                 className="rounded-xl cursor-pointer shadow-lg sm:shadow-none"
                                 alt={data.title}
                                 key={data.id}
@@ -41,12 +20,13 @@ const ProjectGallery = ({id}) => {
                     :
                         <div key={data.id} className="col-span-2 flex justify-center text-center items-center">
                             <img
-                                src={data.images[0].img}
+                                src={image.img}
                                 className="rounded-xl cursor-pointer shadow-lg sm:shadow-none"
                                 alt={data.title}
                                 key={data.id}
                             />
                         </div>
+                ))
             }
         </div>
     );
